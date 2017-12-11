4693ca583750bdb968c9c29fbf703aab557e2a63
Glenn Gartner <ggartner@sonalysts.com>
2017-12-11 10:19:55 -0500

    improve version_two using merge sort

--- assignment/improving_complexity_version_two.rb
+++ assignment/improving_complexity_version_two.rb
@@ -1,4 +1,6 @@
 # This method takes n arrays as input and combine them in sorted ascending  order
+# It was imrpoved by using merge sort 
+
  def poorly_written_ruby(*arrays)
      
      # create an empty combined array
@@ -9,47 +11,53 @@
          combined_array += array
      end
 
-     sorted_array = [combined_array.delete_at(0)]
+     return combined_array
+end
+
+def mergeSort(array)
+    # sorted_array = [array.delete_at(0)]
      
     # perform a merge sort
 	# if the array is 1 item long, consider it sorted and exit  
-    if combined_array.count <= 1
+    if array.count <= 1
         return array
     end
 
-    mid = combined_array.count / 2
-    left = mergesort array.slice(0, mid)
-    right = mergesort array.slice(mid, array.count - mid)
+    mid = array.count / 2
+    left = mergeSort array.slice(0, mid)
+    right = mergeSort array.slice(mid, array.count - mid)
 
-    # 2. Conquer
     array = []
-    offset_a = 0
-    offset_b = 0
+    offset_left = 0
+    offset_right = 0
 
-    while offset_a < left.count && offset_b < right.count
-        a = left[offset_a]
-        b = right[offset_b]
+    while offset_left < left.count && offset_right < right.count
+        a = left[offset_left]
+        b = right[offset_right]
 
         # Take the smallest of the two, and push it on our array
         if a <= b
             array << a
-            offset_a += 1
+            offset_left += 1
         else
             array << b
-            offset_b += 1
+            offset_right += 1
         end
     end
 
     # There is at least one element left in either part_a or part_b (not both)
-    while offset_a < part_a.count
-        array << left[offset_a]
-        offset_a += 1
+    while offset_left < left.count
+        array << left[offset_left]
+        offset_left += 1
     end
 
-    while offset_b < part_b.count
-        array << right[offset_b]
-        offset_b += 1
+    while offset_right < right.count
+        array << right[offset_right]
+        offset_right += 1
     end
 
     return array 
  end
+
+combined_array = poorly_written_ruby([9,2,13,4,10,15,1], [14,3,11,7], [8,12,6,5])
+puts "#{mergeSort(combined_array)}"
