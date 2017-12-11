5e9011984ada7fa011db1a0d74161e853031aba3
Glenn Gartner <ggartner@sonalysts.com>
2017-12-11 10:31:54 -0500

    improve version_three by sorting in-place with bubble-sort

--- assignment/improving_complexity_version_three.rb
+++ assignment/improving_complexity_version_three.rb
@@ -1,28 +1,31 @@
 # This method takes n arrays as input and combine them in sorted ascending  order
+# re-written using bubble sort, which sorts in-place. Saves memory usage 
+
  def poorly_written_ruby(*arrays)
    combined_array = []
    arrays.each do |array|
-     array.each do |value|
-       combined_array << value
-     end
+       combined_array += array
+   end
+   return bubble_sort(combined_array)
+end
+
+def bubble_sort(array)
+   n = array.length
+
+   loop do
+      swapped = false
+
+      (n-1).times do |i|
+         if array[i] > array[i+1]
+            array[i], array[i+1] = array[i+1], array[i]
+            swapped = true
+         end
+      end
+
+      break if not swapped
    end
 
-   sorted_array = [combined_array.delete_at(combined_array.length-1)]
-
-   for val in combined_array
-     i = 0
-     while i < sorted_array.length
-       if val <= sorted_array[i]
-         sorted_array.insert(i, val)
-         break
-       elsif i == sorted_array.length - 1
-         sorted_array << val
-         break
-       end
-       i+=1
-     end
-   end
-
-   # Return the sorted array
-   sorted_array
+   return array
  end
+
+puts "#{poorly_written_ruby([9,12,3,7,5,11], [4,8,1,13], [15,2,10,14,6])}"
